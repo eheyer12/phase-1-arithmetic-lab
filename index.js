c21fba62e6b56ea46c1fea2ff4fefcc4d8567b28
Eduardo Heyer <eduardo.heyer12@gmail.com>
2023-04-10 06:23:34 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,25 @@
 
 // Write your code here
+const num1 = 31;
+const num2 = 2;
+
+function multiply(num1, num2) {
+    
+    return num1 * num2;
+}
+
+function random() {
+    let random_num = Math.ceil(Math.random() * 1000) + 1;
+    return random_num;
+}
+
+function mod(num3, num4) {
+    return (num3 % num4);
+}
+const num3 = 10;
+const num4 = 6;
+
+function max(values) {
+    return Math.max(...values);
+}
+
